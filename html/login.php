39cb14af7c14a580ac96f45ee223adf20101169c
Niklas Lindholm <niklas@nilin.se>
2025-02-01 19:48:00 -0800

    Fix to populate session with user id from database to avoid it having case problems.

--- html/login.php
+++ html/login.php
@@ -1,10 +1,10 @@
 <?php
   require 'db.php';
 
-  $stmt = $conn->prepare("SELECT Name, Password, IsAdmin FROM Users WHERE UserId = ?");
-  $stmt->bind_param("s", $userid);
   $userid = $_POST["userid"];
   $password = $_POST["password"];
+  $stmt = $conn->prepare("SELECT UserId, Name, Password, IsAdmin FROM Users WHERE UserId = ?");
+  $stmt->bind_param("s", $userid);
   $stmt->execute();
   $result = $stmt->get_result();
   if ($result->num_rows > 0) {
@@ -14,7 +14,7 @@
     $pwdhash = "";
   }
   if(password_verify($password, $pwdhash)) {
-    $_SESSION["user"] = $userid;
+    $_SESSION["user"] = $row["UserId"];
     $_SESSION["fullname"] = $row["Name"];
     if($row["IsAdmin"]) {
       $_SESSION["isadmin"] = TRUE;
