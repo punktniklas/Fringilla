00b3fa76c6a20baf26057b27d32dc59f9ee6bd2c
Niklas Lindholm <niklas@nilin.se>
2026-09-21 17:37:15 -0700

    Setting 2026-27 regular as default season.

--- html/db.php
+++ html/db.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $user = array_key_exists("user", $_SESSION) ? $_SESSION["user"] : "";
-$currentSeason = "20252026-3";
+$currentSeason = "20262027-2";
 $selectedSeason = array_key_exists("selectedSeason", $_SESSION) ? $_SESSION["selectedSeason"] : $currentSeason;
 
 require 'dbpw.php';
